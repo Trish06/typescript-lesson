3584cd9f44a4de10f062333ac48d2aad9e25e72d
Trisha Ann Dela Torre <trishaann.recio@gmail.com>
2023-09-06 23:23:22 +0800

    include advance data types in object_type Branch

--- app.js
+++ app.js
@@ -1,14 +1,15 @@
-function addFunction(num1, num2, showResult, message) {
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(message + result);
-    }
-    else {
-        console.log("show result is false");
-    }
+var Access;
+(function (Access) {
+    Access["ADMIN"] = "123";
+    Access[Access["MODERATOR"] = 345] = "MODERATOR";
+    Access[Access["USER"] = 111] = "USER";
+})(Access || (Access = {}));
+var student = {
+    name: 'Juan',
+    age: 19,
+    interest: ['Basketball', 'Reading'],
+    access: Access.ADMIN
+};
+if (student.access === '123') {
+    console.log('is admin');
 }
-var number1 = 5;
-var number2 = 2.4;
-var showResult = true;
-var message = 'Sum is :';
-addFunction(number1, number2, showResult, message);
--- app.ts
+++ app.ts
@@ -1,15 +1,19 @@
-function addFunction (num1 : number, num2 : number, showResult : boolean, message : string) {
-  const result = num1 + num2;
-  if(showResult) {
-    console.log(message + result);
-  } else {
-    console.log("show result is false");
-  }
+enum Access {ADMIN = '123', MODERATOR = 345, USER = 111}
+
+const student: {
+  name: string;
+  age: number;
+  gender?: string;
+  interest: string[];
+  access : Access
+} = {
+  name: 'Juan',
+  age: 19,
+  interest: ['Basketball', 'Reading'],
+  access : Access.ADMIN
+};
+
+if(student.access === '123'){
+  console.log('is admin');
 }
 
-const number1 = 5;
-const number2 = 2.4;
-const showResult = true;
-const message = 'Sum is :'
-
-addFunction(number1, number2, showResult, message)
